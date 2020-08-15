c15776a6d8a837abd87d24398d8e54ef3497e540
Abhimanyu Singh Chittoria <43439448+chittoriabhi@users.noreply.github.com>
2020-08-15 19:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='PM2.5 will be $ {}'.format(output))
+    return render_template('index.html', prediction_text='PM2.5 will be  {}'.format(output))
 
 
 if __name__ == "__main__":
